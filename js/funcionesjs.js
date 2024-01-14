30bccfd1c2e96167a328383ca7700843900e3e5c
Marco <marc.hernandeza@duocuc.cl>
2024-01-14 02:32:37 -0300

    agreg

--- js/funcionesjs.js
+++ js/funcionesjs.js
@@ -3,9 +3,14 @@
 function inicioSesion(){
 
     event.preventDefault();
-    
+
     var usuario = document.getElementById("exampleInputEmail1").value;
 
-    console.log("usuario: "+usuario);
+    var password = document.getElementById("exampleInputPassword1").value;
+
+    if(usuario == ''){
+        alert("El email no puede ir vacio")
+    }
 
 }
+
